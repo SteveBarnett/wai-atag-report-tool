d4a41dbc4b52676f7c77c34c7a71d2672f45fc96
Hidde de Vries <hidde@hiddedevries.nl>
2020-06-26 16:10:05 +0200

    wfawef

--- src/components/NavItem.svelte
+++ src/components/NavItem.svelte
@@ -7,10 +7,20 @@
   function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
     let relativeHref = href.replace(vars.pathPrefix, '');
     const isActive = relativeHref === location.pathname;
+    // ? isCurrent : isPartiallyCurrent || isCurrent;
 
     if (relativeHref === "") {
       relativeHref = "/";
     }
+  console.log('-------');
+    console.log('are rel href and loc name equal', relativeHref === location.pathname);
+    console.log('href', href);
+    console.log('relativeHref', relativeHref);
+    console.log('location', location);
+    console.log('isActive', isActive);
+    console.log('isCurrent', isCurrent);
+
+    debugger;
 
     // The object returned here is spread on the anchor element's attributes
     if (isActive) {
